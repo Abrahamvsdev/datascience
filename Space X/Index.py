54382f45280e37ff3ae08f85a67260892eb7100f
Abrahamvsdev <145706714+Abrahamvsdev@users.noreply.github.com>
2024-10-01 00:56:38 +0200

    1 de octubre
    algunas funciones

--- Space X/Index.py
+++ Space X/Index.py
@@ -1,3 +1,31 @@
-url="https://api.spacexdata.com/v4/launches/past"
-response = requests.get(url)
-response.json()
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+#parece que el import, como en java, se bugea, hay que reiniciar el vs code
+
+# esto pinta todas las columnas del df
+pd.set_option("display.max_columns", None)
+# pinta toda la data en una feature
+pd.set_option("display.max_colwidth",None)
+
+def getBoosterVersion(data):
+    for x in data['rocket']:
+        if x:
+            response = requests.get("https://api.spacexdata.com/v4/rockets/"+str(x)).json()
+            BoosterVersion.append(response['name'])
+
+def getLaunchSite(data):
+    for x in data['launchpad']:
+        if x:
+            response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+            PayloadMass.append(response['logitude'])
+            Latitude.append(response['latitude'])
+            LaunchSite.append(response['name'])
+
+def getPayloadData(data):
+    for load in data['payloads']:
+        if load:
+            response = requests.get("https://api.spacexdata.com/v4/payloads/"+load).json()
+            PayloadMass.append(response['mass_kg'])
+            Orbit.append(response['orbit'])
